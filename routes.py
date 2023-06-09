d20b21e5ec4996ccd00d044b456cde821feb0fc1
anchu435 <60092605@cna-qatar.edu.qa>
2023-06-09 22:37:55 +0300

    added view all recipes function

--- routes.py
+++ routes.py
@@ -1,8 +1,25 @@
 from flask import Blueprint,render_template, request
+import json
+import sys
+main = Blueprint('main',__name__)#routename = main
 
-#routename = main
-main = Blueprint('main',__name__)
+#load recipes.json
+def load_recipes_from_json():
+    with open('recipes.json', 'r') as file:
+        data = json.load(file)
+        return data
 
+recipes = load_recipes_from_json()
+
+
+#homepage
 @main.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    recipes = load_recipes_from_json()
+    #print(recipes, file=sys.stderr)
+    return render_template('index.html',recipes = recipes)
+
+#view recipes
+@main.route('/view',methods=['GET'])
+def view_recipe():
+    return render_template('viewrecipes.html')
