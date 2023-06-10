b1bf7a37b9ed7b04378affefdd4ba388001a6991
Ilsah <60099115@udst.edu.qa>
2023-06-10 04:26:40 +0300

    Added view by id function

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template, request
+from flask import Blueprint,render_template, request, url_for
 import json
 import sys
 main = Blueprint('main',__name__)#routename = main
@@ -9,6 +9,16 @@ def load_recipes_from_json():
         data = json.load(file)
         return data
 
+def get_by_id(id):
+     with open('recipes.json', 'r') as file:
+        data = json.load(file)
+        for i in data:
+            if i['id'] == id*1:
+                print(i, file=sys.stderr)
+                return i
+        else:
+            return "not found"
+     
 recipes = load_recipes_from_json()
 
 
@@ -20,6 +30,8 @@ def home():
     return render_template('index.html',recipes = recipes)
 
 #view recipes
-@main.route('/view',methods=['GET'])
-def view_recipe():
-    return render_template('viewrecipes.html')
+@main.route('/view/<int:id>',methods=['GET'])
+def view_recipe(id):
+    recipe = get_by_id(id)
+    print(recipe, file=sys.stderr)
+    return render_template('viewrecipes.html',recipes = recipe)
