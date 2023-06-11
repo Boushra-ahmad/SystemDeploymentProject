cf74b220fa1eaf1d588f69b0455ddbcc6c20bbc0
shifa <60099580@cna-qatar.edu.qa>
2023-06-11 21:38:43 +0300

    Added import recipe function, html page and modified add-recipe.html to contain the import file line

--- routes.py
+++ routes.py
@@ -3,12 +3,15 @@ import json
 import sys
 from datetime import datetime
 import os
+import csv
+import pandas as pd
 
 main = Blueprint('main',__name__)#routename = main
 
 
 # Specify the directory where the images will be saved
 UPLOAD_FOLDER = 'static/images'
+UPLOAD_FOLDER2 = 'static/files'
 
 #load recipes.json
 def load_recipes_from_json():
@@ -25,6 +28,17 @@ def get_by_id(id):
                 return i
         else:
             return "not found"
+        
+#Process recipe data from .csv or .xlsx files
+def process_recipe_row(rows, existingRecipes):
+    # Generate a unique ID for the new recipe
+    rows['id'] = len(existingRecipes) + 1
+    rows['instructions'] = list(rows['instructions'].split(". "))
+    rows['instructions'] = [instruction + '.' if index != len(rows['instructions']) - 1 else instruction for index, instruction in enumerate(rows['instructions'])]
+    rows['ingredients'] = list(rows['ingredients'].split(', '))
+    # Add the new recipe to the existing recipes
+    existingRecipes.append(rows)
+
      
 recipes = load_recipes_from_json()
 
@@ -92,3 +106,47 @@ def view_recipe(id):
     recipe = get_by_id(id)
     #print(recipe, file=sys.stderr)
     return render_template('viewrecipes.html',recipes = recipe)
+
+#import recipes
+@main.route('/import', methods=['GET','POST'])
+def import_recipe():
+    if request.method == 'POST':
+        # data = []
+        if 'import' in request.files:
+            csvFile = request.files['import']
+            filename = csvFile.filename
+            print(filename)
+            csvFile.save(os.path.join(UPLOAD_FOLDER2, filename))
+            if csvFile.filename.endswith('.csv'):
+                data = csvFile.read().decode('utf-8')
+            elif filename.endswith('.xlsx'):
+                # Handle XLSX file
+                data = csvFile.read()
+                
+            # Open a json writer, and use the json.dumps() function to dump data      
+            with open('recipes.json', 'r') as jsonf:
+                existingRecipes = json.load(jsonf)
+
+            # Open a csv reader called DictReader
+            with open(UPLOAD_FOLDER2+'/'+filename) as csvf:
+                if filename.endswith('.csv'):
+                    # Convert each row into a dictionary and add it to data
+                    csvReader = csv.DictReader(csvf)    
+                    for rows in csvReader:
+                        process_recipe_row(rows, existingRecipes)
+                
+                elif filename.endswith('.xlsx'):
+                    df = pd.read_excel(data, engine='openpyxl')
+                    # Convert DataFrame to a list of dictionaries
+                    recipes = df.to_dict(orient='records')  
+                    for recipe in recipes:
+                        process_recipe_row(recipe, existingRecipes) 
+            
+            # Write the updated recipes back to the JSON file
+            with open('recipes.json', 'w') as jsonFile:
+                jsonFile.write(json.dumps(existingRecipes, indent=4))
+                
+            return redirect(url_for('main.home'))
+        return 'Invalid file'
+    return render_template('importRecipes.html')
+
