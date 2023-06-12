7e303efd86eb67f2755630430bbb8c9edd7473a5
shifa004 <60099580@udst.edu.qa>
2023-06-12 21:26:52 +0300

    Added folders to save xlsx to csv converted file, and images. Modified import function

--- routes.py
+++ routes.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import os
 import csv
 import pandas as pd
+import requests
 
 main = Blueprint('main',__name__)#routename = main
 
@@ -36,7 +37,14 @@ def convert_xlsx_to_csv(xlsx_file, csv_file):
     # Write the DataFrame to CSV file
     data_frame.to_csv(csv_file, index=False)
 
-     
+def download_image(url, save_path):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    
+    with open(save_path, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+
 recipes = load_recipes_from_json()
 
 
@@ -114,22 +122,36 @@ def import_recipe():
             csvFile.save(os.path.join(UPLOAD_FOLDER2, filename))
             if filename.endswith('.xlsx'):
                 # Handle XLSX file
-                csv_file = 'static/files/importedRecipes.csv'
-                convert_xlsx_to_csv('static/files/' + filename, csv_file)
+                csvFile = 'static/files/importedRecipes.csv'
+                convert_xlsx_to_csv('static/files/' + filename, csvFile)
             
             # Open a json writer, and use the json.dumps() function to dump data      
             with open('recipes.json', 'r') as jsonf:
                 existingRecipes = json.load(jsonf)
 
             # Open a csv reader called DictReader
-            with open(csv_file) as csvf:
-                csvReader = csv.DictReader(csvf)    
-                for rows in csvReader:
+            with open(csvFile) as csvf:
+                save_directory = 'static/images'             
+
+                # Convert each row into a dictionary and add it to data
+                csvReader = csv.DictReader(csvf)                    
+                for rows in csvReader:                    
+                    image_filename = rows['name'] + '.jpg'
+                    
+                    # Combine the save directory and image filename to create the save path
+                    save_path = os.path.join(save_directory, image_filename)
+                    
+                    # Create the save directory if it doesn't exist
+                    os.makedirs(save_directory, exist_ok=True)
+                    download_image(rows['image'], save_path)
+                    
                     # Generate a unique ID for the new recipe
                     rows['id'] = len(existingRecipes) + 1
                     rows['instructions'] = list(rows['instructions'].split(". "))
                     rows['instructions'] = [instruction + '.' if index != len(rows['instructions']) - 1 else instruction for index, instruction in enumerate(rows['instructions'])]
                     rows['ingredients'] = list(rows['ingredients'].split(', '))
+                    rows['image'] = rows['name'] + '.jpg'
+                    
                     # Add the new recipe to the existing recipes
                     existingRecipes.append(rows)
 
